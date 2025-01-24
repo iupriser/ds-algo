2a65858055da0e55a48d25f835c0ac0586876989
iupriser <ideepak.sharma2196@gmail.com>
2025-01-24 14:52:59 +0530

    added comments

--- dsalgo/src/linkedList/AddTwoNumber.java
+++ dsalgo/src/linkedList/AddTwoNumber.java
@@ -6,18 +6,22 @@ import static linkedList.Node.printLL;
 //https://leetcode.com/problems/add-two-numbers/description/
 public class AddTwoNumber {
     public static void main(String[] args) {
-        // l1 = 345
-        int[] num1 = {3, 4, 5};
+        int[] num1 = {2, 4, 3};
+        //The digits are stored in reverse order
+        // 3<-4<-2 , 3(MSD), 2(head and LSD)
         Node l1 = createLL(num1);
-        // l2 = 645
-        int[] num2 = {6, 4, 5};
+        int[] num2 = {5, 6, 4};
+        //The digits are stored in reverse order
+        // 4<-6<-5, 4(MSD), 5(head and LSD)
         Node l2 = createLL(num2);
+        // 342+465=807, 8<-0<-7,  8(MSD), 7(head and LSD)
         Node summedList = addTwoNumbers(l1, l2);
         printLL(summedList);
     }
 
+    // create a new LL to store sum for each digit as nodes
     private static Node addTwoNumbers(Node l1, Node l2) {
-        Node dummy = new Node();
+        Node dummy = new Node(-1);
         Node tmp = dummy;
         int carry = 0;
         while (l1 != null || l2 != null || carry == 1) {
@@ -36,6 +40,5 @@ public class AddTwoNumber {
             tmp = tmp.next;
         }
         return dummy.next;
-
     }
 }
