a98acb323e47426d65fb110256903fe5380975c3
iupriser <ideepak.sharma2196@gmail.com>
2025-05-24 16:23:32 +0530

    cycleDetectUndirectedBFS

--- dsalgo/src/graph/DetectCycleUndirectedBFS.java
+++ dsalgo/src/graph/DetectCycleUndirectedBFS.java
@@ -1,4 +1,63 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DetectCycleUndirectedBFS {
+    public static void main(String[] args) {
+        int[][] edges = {{0, 1}, {0, 2}, {1, 2}, {2, 3}};
+        int V = 4;
+        DetectCycleUndirectedBFS obj = new DetectCycleUndirectedBFS();
+        boolean isCyclePresent = obj.isCycle(4, edges);
+        System.out.println("is cycle present : " + isCyclePresent);
+    }
+
+    public boolean isCycle(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = constructAdjList(V, edges);
+        boolean[] vis = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                if (checkForCycle(i, V, adj, vis)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkForCycle(int src, int V, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+        // {vertex, parentOfVertex}
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src, -1));
+        vis[src] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.peek().first;
+            int parent = q.peek().second;
+            q.remove();
+
+            for (int adjNode : adj.get(node)) {
+                if (!vis[adjNode]) {
+                    vis[adjNode] = true;
+                    q.add(new Pair(adjNode, node));
+                }
+                // if adjacent node is visited and is not its own parent node
+                else if (parent != adjNode) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<ArrayList<Integer>> constructAdjList(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        return adj;
+    }
 }
