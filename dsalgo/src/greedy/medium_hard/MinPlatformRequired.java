36376e0d8bfa07023786a5a6c5567b93cacebcb7
iupriser <Lenova007@#>
2025-09-26 17:22:17 +0530

    greedyalgo

---
+++ dsalgo/src/greedy/medium_hard/MinPlatformRequired.java
@@ -0,0 +1,56 @@
+package greedy.medium_hard;
+
+import java.util.Arrays;
+
+public class MinPlatformRequired {
+    private int minPlatformBrute(int[] arr, int[] dep) {
+        int n = arr.length;
+        int maxCount = 0;
+        for (int i = 0; i < n; i++) {
+            // initially one platform is needed
+            int cnt = 1;
+            for (int j = i + 1; j < n; j++) {
+                // overlapping/intersecting checks
+                if ((arr[i] <= arr[j] && dep[i] <= dep[j]) ||
+                        (arr[i] <= arr[j] && dep[i] >= dep[j]) ||
+                        (arr[i] >= arr[j] && dep[i] <= dep[j]) ||
+                        (arr[i] >= arr[j] && dep[i] >= dep[j])) {
+                    cnt++;
+                }
+                maxCount = Math.max(maxCount, cnt);
+            }
+        }
+        return maxCount;
+    }
+
+    private int minPlatformOptimal(int[] arr, int[] dep) {
+        int numTrains = arr.length;
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int maxPlatforms = 0;
+        int currPlatformReq = 0;
+        int p1 = 0, p2 = 0;
+        while (p1 < numTrains) {
+            if (arr[p1] <= dep[p2]) {
+                currPlatformReq++;
+                p1++;
+            } else {
+                currPlatformReq--;
+                p2++;
+            }
+            maxPlatforms = Math.max(maxPlatforms, currPlatformReq);
+        }
+        return maxPlatforms;
+    }
+
+    public static void main(String[] args) {
+        MinPlatformRequired obj = new MinPlatformRequired();
+        int[] arrival = {900, 945, 955, 1100, 1500};
+        int[] departure = {920, 1200, 1130, 1150, 2000};
+        int minPlatform = obj.minPlatformOptimal(arrival, departure);
+        System.out.println("minimum number of platforms required : " + minPlatform);
+        System.out.println("--------------------");
+        minPlatform = obj.minPlatformOptimal(arrival, departure);
+        System.out.println("minimum number of platforms required : " + minPlatform);
+    }
+}
